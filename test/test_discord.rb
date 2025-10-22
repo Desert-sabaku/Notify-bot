be1f32321f96bd22852c78f1ebe0fa19007c9af5
SANADA Euki <yuu.mat.930@gmail.com>
2025-10-22 13:44:01 +0900

    fix: remove unused open_auth_url helper and clean up test setup for Discord and OAuth

--- test/test_discord.rb
+++ test/test_discord.rb
@@ -3,6 +3,20 @@ require_relative "test_helper"
 class TestDiscordHelpers < Minitest::Test
   include TestHelper
 
+  def setup
+    setup_env_vars
+    # Ensure Syodosima constants reflect the test env
+    Syodosima.send(:remove_const, :DISCORD_BOT_TOKEN) if Syodosima.const_defined?(:DISCORD_BOT_TOKEN)
+    Syodosima.const_set(:DISCORD_BOT_TOKEN, ENV["DISCORD_BOT_TOKEN"])
+
+    Syodosima.send(:remove_const, :DISCORD_CHANNEL_ID) if Syodosima.const_defined?(:DISCORD_CHANNEL_ID)
+    Syodosima.const_set(:DISCORD_CHANNEL_ID, ENV["DISCORD_CHANNEL_ID"])
+  end
+
+  def teardown
+    cleanup_env_vars
+  end
+
   def test_create_discord_bot_and_deliver
     bot_spy = BotSpy.new
     # ensure create_discord_bot returns a Discordrb::Bot-like object
